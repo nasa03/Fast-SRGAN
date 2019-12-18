eb104991548e4db755408e3432d52366f6a83dea
HasnainRaz <hasnain3257@gmail.com>
2019-12-18 21:38:02 +0500

    Update model and training

--- main.py
+++ main.py
@@ -6,51 +6,12 @@ import os
 
 parser = ArgumentParser()
 parser.add_argument('--image_dir', default='/Users/hasty/Datasets/div2k', type=str, help='Path to high resolution image directory.')
-parser.add_argument('--batch_size', default=8, type=int, help='Batch size for training.')
-parser.add_argument('--epochs', default=1, type=int, help='Number of epochs for training')
+parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training.')
+parser.add_argument('--epochs', default=100000, type=int, help='Number of epochs for training')
 parser.add_argument('--hr_size', default=128, type=int, help='Low resolution input size.')
 parser.add_argument('--lr', default=1e-4, type=float, help='Learning rate for optimizers.')
 parser.add_argument('--save_iter', default=200, type=int,
                     help='The number of iterations to save the tensorboard summaries and models.')
-
-
-@tf.function
-def pretrain_step(model, x, y):
-    """
-    Single step of generator pre-training.
-    Args:
-        model: A model object with a tf keras compiled generator.
-        x: The low resolution image tensor.
-        y: The high resolution image tensor.
-    """
-    with tf.GradientTape() as tape:
-        fake_hr = model.generator(x)
-        loss_mse = tf.math.reduce_mean(tf.math.reduce_mean(tf.square(y - fake_hr), axis=[1, 2, 3]))
-
-    grads = tape.gradient(loss_mse, model.generator.trainable_variables)
-    model.gen_optimizer.apply_gradients(zip(grads, model.generator.trainable_variables))
-
-    return loss_mse
-
-
-def pretrain_generator(model, dataset, writer):
-    """Function that pretrains the generator slightly, to avoid local minima.
-    Args:
-        model: The keras model to train.
-        dataset: A tf dataset object of low and high res images to pretrain over.
-        writer: A summary writer object.
-    Returns:
-        None
-    """
-    with writer.as_default():
-        iteration = 0
-        for _ in range(1):
-            for x, y in dataset:
-                loss = pretrain_step(model, x, y)
-                if iteration % 20 == 0:
-                    tf.summary.scalar('MSE Loss', loss, step=tf.cast(iteration, tf.int64))
-                    writer.flush()
-                iteration += 1
 
 
 @tf.function
@@ -106,7 +67,7 @@ def train_step(model, x, y):
     disc_grads = disc_tape.gradient(d_loss, model.discriminator.trainable_variables)
     model.disc_optimizer.apply_gradients(zip(disc_grads, model.discriminator.trainable_variables))
 
-    return d_loss, adv_loss, content_loss, mse_loss
+    return valid_loss, fake_loss, adv_loss, content_loss, mse_loss
 
 
 def train(model, dataset, log_iter, writer):
@@ -123,13 +84,14 @@ def train(model, dataset, log_iter, writer):
     with writer.as_default():
         # Iterate over dataset
         for x, y in dataset:
-            disc_loss, adv_loss, content_loss, mse_loss = train_step(model, x, y)
+            dReal, dFake, adv_loss, content_loss, mse_loss = train_step(model, x, y)
             # Log tensorboard summaries if log iteration is reached.
             if model.iterations % log_iter == 0:
                 tf.summary.scalar('Adversarial Loss', adv_loss, step=model.iterations)
                 tf.summary.scalar('Content Loss', content_loss, step=model.iterations)
                 tf.summary.scalar('MSE Loss', mse_loss, step=model.iterations)
-                tf.summary.scalar('Discriminator Loss', disc_loss, step=model.iterations)
+                tf.summary.scalar('Discriminator Loss Real', dReal, step=model.iterations)
+                tf.summary.scalar('Discriminator Loss Fake', dReal, step=model.iterations)
                 tf.summary.image('Low Res', tf.cast(255 * x, tf.uint8), step=model.iterations)
                 tf.summary.image('High Res', tf.cast(255 * (y + 1.0) / 2.0, tf.uint8), step=model.iterations)
                 tf.summary.image('Generated', tf.cast(255 * (model.generator.predict(x) + 1.0) / 2.0, tf.uint8),
@@ -151,17 +113,11 @@ def main():
     # Create the tensorflow dataset.
     ds = DataLoader(args.image_dir, args.hr_size).dataset(args.batch_size)
 
-    # Define the directory for saving pretrainig loss tensorboard summary.
-    pretrain_summary_writer = tf.summary.create_file_writer('logs/pretrain')
-
     # Define the directory for saving the SRGAN training tensorbaord summary.
     train_summary_writer = tf.summary.create_file_writer('logs/train')
 
     # Initialize the GAN object.
     gan = FastSRGAN(args)
-
-    # Run pre-training.
-    pretrain_generator(gan, ds, pretrain_summary_writer)
 
     # Run training.
     for _ in range(args.epochs):
