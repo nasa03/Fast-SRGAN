9a997f6704ed00cd7675d0bbb186db1407d8bc3d
HasnainRaz <hasnain3257@gmail.com>
2019-12-22 04:10:23 +0500

    Revert back to faster

--- main.py
+++ main.py
@@ -5,13 +5,52 @@ import tensorflow as tf
 import os
 
 parser = ArgumentParser()
-parser.add_argument('--image_dir', default='/Users/hasty/Datasets/div2k', type=str, help='Path to high resolution image directory.')
-parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training.')
-parser.add_argument('--epochs', default=100000, type=int, help='Number of epochs for training')
-parser.add_argument('--hr_size', default=128, type=int, help='Low resolution input size.')
+parser.add_argument('--image_dir', type=str, help='Path to high resolution image directory.')
+parser.add_argument('--batch_size', default=8, type=int, help='Batch size for training.')
+parser.add_argument('--epochs', default=1, type=int, help='Number of epochs for training')
+parser.add_argument('--hr_size', default=384, type=int, help='Low resolution input size.')
 parser.add_argument('--lr', default=1e-4, type=float, help='Learning rate for optimizers.')
 parser.add_argument('--save_iter', default=200, type=int,
                     help='The number of iterations to save the tensorboard summaries and models.')
+
+
+@tf.function
+def pretrain_step(model, x, y):
+    """
+    Single step of generator pre-training.
+    Args:
+        model: A model object with a tf keras compiled generator.
+        x: The low resolution image tensor.
+        y: The high resolution image tensor.
+    """
+    with tf.GradientTape() as tape:
+        fake_hr = model.generator(x)
+        loss_mse = tf.keras.losses.MeanSquaredError()(y, fake_hr)
+
+    grads = tape.gradient(loss_mse, model.generator.trainable_variables)
+    model.gen_optimizer.apply_gradients(zip(grads, model.generator.trainable_variables))
+
+    return loss_mse
+
+
+def pretrain_generator(model, dataset, writer):
+    """Function that pretrains the generator slightly, to avoid local minima.
+    Args:
+        model: The keras model to train.
+        dataset: A tf dataset object of low and high res images to pretrain over.
+        writer: A summary writer object.
+    Returns:
+        None
+    """
+    with writer.as_default():
+        iteration = 0
+        for _ in range(1):
+            for x, y in dataset:
+                loss = pretrain_step(model, x, y)
+                if iteration % 20 == 0:
+                    tf.summary.scalar('MSE Loss', loss, step=tf.cast(iteration, tf.int64))
+                    writer.flush()
+                iteration += 1
 
 
 @tf.function
@@ -25,62 +64,38 @@ def train_step(model, x, y):
     Returns:
         d_loss: The mean loss of the discriminator.
     """
-    valid = tf.ones((x.shape[0], 1))
+    # Label smoothing for better gradient flow
+    valid = tf.ones((x.shape[0],) + model.disc_patch)
+    fake = tf.zeros((x.shape[0],) + model.disc_patch)
 
-    with tf.GradientTape() as gen_tape:
-        # Get fake image:
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        # From low res. image generate high res. version
         fake_hr = model.generator(x)
 
-        # Get discriminator predictions:
-        valid_predictions = model.discriminator(y)
-        fake_predictions = model.discriminator(fake_hr)
+        # Train the discriminators (original images = real / generated = Fake)
+        valid_prediction = model.discriminator(y)
+        fake_prediction = model.discriminator(fake_hr)
 
-        # Get generator losses:
-        valid_loss = valid_predictions - tf.math.reduce_mean(fake_predictions, axis=0, keepdims=True) + valid
-        fake_loss = fake_predictions - tf.math.reduce_mean(valid_predictions, axis=0, keepdims=True) - valid
-        valid_loss = tf.math.reduce_mean(tf.square(valid_loss))
-        fake_loss = tf.math.reduce_mean(tf.square(fake_loss))
-        adv_loss = 5e-3 * tf.divide(valid_loss + fake_loss, 2.0)
+        # Generator loss
         content_loss = model.content_loss(y, fake_hr)
-        mse_loss = 1e-2 * tf.reduce_mean(tf.reduce_mean(tf.math.abs(fake_hr - y), axis=[1, 2, 3]))
+        adv_loss = 1e-3 * tf.keras.losses.BinaryCrossentropy()(valid, fake_prediction)
+        mse_loss = tf.keras.losses.MeanSquaredError()(y, fake_hr)
         perceptual_loss = content_loss + adv_loss + mse_loss
+
+        # Discriminator loss
+        valid_loss = tf.keras.losses.BinaryCrossentropy()(valid, valid_prediction)
+        fake_loss = tf.keras.losses.BinaryCrossentropy()(fake, fake_prediction)
+        d_loss = tf.add(valid_loss, fake_loss)
 
     # Backprop on Generator
     gen_grads = gen_tape.gradient(perceptual_loss, model.generator.trainable_variables)
     model.gen_optimizer.apply_gradients(zip(gen_grads, model.generator.trainable_variables))
 
-    with tf.GradientTape() as disc_tape:
-        # Get fake image:
-        fake_hr = model.generator(x)
-
-        # Get discriminator predictions:
-        fake_predictions = model.discriminator(fake_hr)
-        valid_predictions = model.discriminator(y)
-
-        # calculate loss of the discriminator
-        fake_loss = fake_predictions - tf.math.reduce_mean(valid_predictions, axis=0, keepdims=True) + valid
-        fake_loss = tf.math.reduce_mean(tf.square(fake_loss)) * 0.5
-
-    disc_grads_fake = disc_tape.gradient(fake_loss, model.discriminator.trainable_variables)
-    model.disc_optimizer.apply_gradients(zip(disc_grads_fake, model.discriminator.trainable_variables))
-
-    with tf.GradientTape() as disc_tape:
-        # Get fake image:
-        fake_hr = model.generator(x)
-
-        # Get discriminator predictions:
-        fake_predictions = model.discriminator(fake_hr)
-        valid_predictions = model.discriminator(y)
-
-        # Calculate loss of discriminator
-        valid_loss = valid_predictions - tf.math.reduce_mean(fake_predictions, axis=0, keepdims=True) - valid
-        valid_loss = tf.math.reduce_mean(tf.square(valid_loss)) * 0.5
-
     # Backprop on Discriminator
-    disc_grads_valid = disc_tape.gradient(valid_loss, model.discriminator.trainable_variables)
-    model.disc_optimizer.apply_gradients(zip(disc_grads_valid, model.discriminator.trainable_variables))
+    disc_grads = disc_tape.gradient(d_loss, model.discriminator.trainable_variables)
+    model.disc_optimizer.apply_gradients(zip(disc_grads, model.discriminator.trainable_variables))
 
-    return valid_loss, fake_loss, adv_loss, content_loss, mse_loss
+    return d_loss, adv_loss, content_loss, mse_loss
 
 
 def train(model, dataset, log_iter, writer):
@@ -90,25 +105,25 @@ def train(model, dataset, log_iter, writer):
         model: An object that contains tf keras compiled generator and
                discriminator models.
         dataset: A tf data object that contains low and high res images.
-        log_iter: Number of iterations after which to add logs in 
+        log_iter: Number of iterations after which to add logs in
                   tensorboard.
         writer: Summary writer
     """
     with writer.as_default():
         # Iterate over dataset
         for x, y in dataset:
-            dReal, dFake, adv_loss, content_loss, mse_loss = train_step(model, x, y)
+            disc_loss, adv_loss, content_loss, mse_loss = train_step(model, x, y)
             # Log tensorboard summaries if log iteration is reached.
             if model.iterations % log_iter == 0:
                 tf.summary.scalar('Adversarial Loss', adv_loss, step=model.iterations)
                 tf.summary.scalar('Content Loss', content_loss, step=model.iterations)
                 tf.summary.scalar('MSE Loss', mse_loss, step=model.iterations)
-                tf.summary.scalar('Discriminator Loss Real', dReal, step=model.iterations)
-                tf.summary.scalar('Discriminator Loss Fake', dFake, step=model.iterations)
-                tf.summary.image('Low Res', tf.cast(255 * x, tf.uint8), step=model.iterations)
-                tf.summary.image('High Res', tf.cast(255 * y, tf.uint8), step=model.iterations)
-                tf.summary.image('Generated', tf.cast(255 * model.generator.predict(x), tf.uint8),
+                tf.summary.scalar('Discriminator Loss', disc_loss, step=model.iterations)
+                tf.summary.image('Low Res', tf.cast(127.5 * (x + 1.0), tf.uint8), step=model.iterations)
+                tf.summary.image('High Res', tf.cast(127.5 * (y + 1.0), tf.uint8), step=model.iterations)
+                tf.summary.image('Generated', tf.cast(127.5 * (model.generator.predict(x) + 1.0), tf.uint8),
                                  step=model.iterations)
+                tf.summary.image('BiCubic', tf.cast(tf.image.resize((x + 1.0) * 127.5, method='bicubic'), tf.uint8), step=model.iterations)
                 model.generator.save('models/generator.h5')
                 model.discriminator.save('models/discriminator.h5')
                 writer.flush()
@@ -126,11 +141,17 @@ def main():
     # Create the tensorflow dataset.
     ds = DataLoader(args.image_dir, args.hr_size).dataset(args.batch_size)
 
-    # Define the directory for saving the SRGAN training tensorbaord summary.
-    train_summary_writer = tf.summary.create_file_writer('logs/train')
-
     # Initialize the GAN object.
     gan = FastSRGAN(args)
+
+    # Define the directory for saving pretrainig loss tensorboard summary.
+    pretrain_summary_writer = tf.summary.create_file_writer('logs/pretrain')
+
+    # Run pre-training.
+    pretrain_generator(gan, ds, pretrain_summary_writer)
+
+    # Define the directory for saving the SRGAN training tensorbaord summary.
+    train_summary_writer = tf.summary.create_file_writer('logs/train')
 
     # Run training.
     for _ in range(args.epochs):
--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
 import tensorflow as tf
+from vgg import VGG19
 
 
 class FastSRGAN(object):
@@ -22,20 +23,20 @@ class FastSRGAN(object):
         self.iterations = 0
 
         # Number of inverted residual blocks in the mobilenet generator
-        self.n_residual_blocks = 12
+        self.n_residual_blocks = 6
 
         # Define a learning rate decay schedule.
         self.gen_schedule = keras.optimizers.schedules.ExponentialDecay(
             args.lr,
-            decay_steps=100000,
-            decay_rate=0.5,
+            decay_steps=50000,
+            decay_rate=0.1,
             staircase=True
         )
 
         self.disc_schedule = keras.optimizers.schedules.ExponentialDecay(
             args.lr,  # TTUR - Two Time Scale Updates
             decay_steps=50000,
-            decay_rate=0.5,
+            decay_rate=0.1,
             staircase=True
         )
 
@@ -52,23 +53,22 @@ class FastSRGAN(object):
         self.disc_patch = (patch, patch, 1)
 
         # Number of filters in the first layer of G and D
-        self.gf = 32  # Realtime Image Enhancement GAN Galteri et al.
+        self.gf = 24  # Realtime Image Enhancement GAN Galteri et al.
         self.df = 32
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
 
-        # Build and compile the generator for pretraining.
+        # Build and compile the generator for training.
         self.generator = self.build_generator()
 
     @tf.function
     def content_loss(self, hr, sr):
-        sr = keras.applications.vgg19.preprocess_input(sr * 255)
-        hr = keras.applications.vgg19.preprocess_input(hr * 255)
-        sr_features = self.vgg(sr)
-        hr_features = self.vgg(hr)
-        loss = tf.math.reduce_mean(tf.math.reduce_mean(tf.math.abs(sr_features - hr_features), axis=[1, 2, 3]))
-        return loss
+        sr = keras.applications.vgg19.preprocess_input(((sr + 1.0) * 255) / 2.0)
+        hr = keras.applications.vgg19.preprocess_input(((hr + 1.0) * 255) / 2.0)
+        sr_features = self.vgg(sr) / 12.75
+        hr_features = self.vgg(hr) / 12.75
+        return tf.keras.losses.MeanSquaredError()(hr_features, sr_features)
 
     def build_vgg(self):
         """
@@ -76,7 +76,7 @@ class FastSRGAN(object):
         third block of the model
         """
         # Get the vgg network. Extract features from Block 5, last convolution.
-        vgg = keras.applications.VGG19(weights="imagenet", input_shape=self.hr_shape, include_top=False)
+        vgg = VGG19(weights="imagenet", input_shape=self.hr_shape, include_top=False)
         vgg.trainable = False
         for layer in vgg.layers:
             layer.trainable = False
@@ -90,39 +90,92 @@ class FastSRGAN(object):
         """Build the generator that will do the Super Resolution task.
         Based on the Mobilenet design. Idea from Galteri et al."""
 
-        def residual_block(inputs, filters):
+        def _make_divisible(v, divisor, min_value=None):
+            if min_value is None:
+                min_value = divisor
+            new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+            # Make sure that round down does not go down by more than 10%.
+            if new_v < 0.9 * v:
+                new_v += divisor
+            return new_v
+
+        def residual_block(inputs, filters, block_id, expansion=6, stride=1, alpha=1.0):
             """Inverted Residual block that uses depth wise convolutions for parameter efficiency.
             Args:
                 inputs: The input feature map.
                 filters: Number of filters in each convolution in the block.
+                block_id: An integer specifier for the id of the block in the graph.
+                expansion: Channel expansion factor.
+                stride: The stride of the convolution.
+                alpha: Depth expansion factor.
             Returns:
                 x: The output of the inverted residual block.
             """
-            x = keras.layers.Conv2D(filters * 6, kernel_size=1, strides=1, padding='same')(inputs)
-            x = keras.layers.BatchNormalization()(x)
-            x = keras.layers.PReLU(shared_axes=[1, 2])(x)
+            channel_axis = 1 if keras.backend.image_data_format() == 'channels_first' else -1
 
-            x = keras.layers.DepthwiseConv2D(depth_multiplier=1, kernel_size=3, strides=1, padding='same')(x)
-            x = keras.layers.BatchNormalization()(x)
-            x = keras.layers.PReLU(shared_axes=[1, 2])(x)
+            in_channels = keras.backend.int_shape(inputs)[channel_axis]
+            pointwise_conv_filters = int(filters * alpha)
+            pointwise_filters = _make_divisible(pointwise_conv_filters, 8)
+            x = inputs
+            prefix = 'block_{}_'.format(block_id)
 
-            x = keras.layers.Conv2D(filters, kernel_size=1, strides=1, padding='same')(x)
-            x = keras.layers.BatchNormalization()(x)
+            if block_id:
+                # Expand
+                x = keras.layers.Conv2D(expansion * in_channels,
+                                        kernel_size=1,
+                                        padding='same',
+                                        use_bias=True,
+                                        activation=None,
+                                        name=prefix + 'expand')(x)
+                x = keras.layers.BatchNormalization(axis=channel_axis,
+                                                    epsilon=1e-3,
+                                                    momentum=0.999,
+                                                    name=prefix + 'expand_BN')(x)
+                x = keras.layers.Activation('relu', name=prefix + 'expand_relu')(x)
+            else:
+                prefix = 'expanded_conv_'
 
-            x = keras.layers.Add()([x, inputs])
+            # Depthwise
+            x = keras.layers.DepthwiseConv2D(kernel_size=3,
+                                             strides=stride,
+                                             activation=None,
+                                             use_bias=True,
+                                             padding='same' if stride == 1 else 'valid',
+                                             name=prefix + 'depthwise')(x)
+            x = keras.layers.BatchNormalization(axis=channel_axis,
+                                                epsilon=1e-3,
+                                                momentum=0.999,
+                                                name=prefix + 'depthwise_BN')(x)
 
+            x = keras.layers.Activation('relu', name=prefix + 'depthwise_relu')(x)
+
+            # Project
+            x = keras.layers.Conv2D(pointwise_filters,
+                                    kernel_size=1,
+                                    padding='same',
+                                    use_bias=True,
+                                    activation=None,
+                                    name=prefix + 'project')(x)
+            x = keras.layers.BatchNormalization(axis=channel_axis,
+                                                epsilon=1e-3,
+                                                momentum=0.999,
+                                                name=prefix + 'project_BN')(x)
+
+            if in_channels == pointwise_filters and stride == 1:
+                return keras.layers.Add(name=prefix + 'add')([inputs, x])
             return x
 
-        def deconv2d(u, filters):
+        def deconv2d(layer_input):
             """Upsampling layer to increase height and width of the input.
             Uses PixelShuffle for upsampling.
             Args:
-                u: The input tensor to upsample.
-                filters: The numnber of filters.
+                layer_input: The input tensor to upsample.
+                filters: Numbers of expansion filters.
             Returns:
                 u: Upsampled input by a factor of 2.
             """
-            u = keras.layers.Conv2DTranspose(filters, kernel_size=3, strides=2, padding='same')(u)
+            u = keras.layers.UpSampling2D(size=2, interpolation='bilinear')(layer_input)
+            u = keras.layers.DepthwiseConv2D(kernel_size=3, strides=1, padding='same')(u)
             u = keras.layers.PReLU(shared_axes=[1, 2])(u)
             return u
 
@@ -135,30 +188,21 @@ class FastSRGAN(object):
         c1 = keras.layers.PReLU(shared_axes=[1, 2])(c1)
 
         # Propogate through residual blocks
-        r = residual_block(c1, self.gf)
+        r = residual_block(c1, self.gf, 0)
         for idx in range(1, self.n_residual_blocks):
-            r = residual_block(r, self.gf)
+            r = residual_block(r, self.gf, idx)
 
         # Post-residual block
         c2 = keras.layers.Conv2D(self.gf, kernel_size=3, strides=1, padding='same')(r)
         c2 = keras.layers.BatchNormalization()(c2)
-        
+        c2 = keras.layers.Add()([c2, c1])
+
         # Upsampling
-        u1 = deconv2d(c2, self.gf)
-        u2 = deconv2d(u1, self.gf)
+        u1 = deconv2d(c2)
+        u2 = deconv2d(u1)
 
-        # Post upscaling convolution
-        u2 = keras.layers.Conv2D(self.gf, kernel_size=3, strides=1, padding='same')(u2)
-        u2 = keras.layers.PReLU(shared_axes=[1, 2])(u2)
-
-        # upsample the input image:
-        up_image = keras.layers.UpSampling2D(size=4, interpolation='bilinear')(img_lr)
-
-        # Generate high resolution residual
-        residual = keras.layers.Conv2D(3, kernel_size=3, strides=1, padding='same', activation=None)(u2)
-
-        # Add image and residual:
-        gen_hr = keras.layers.Add()([residual, up_image])
+        # Generate high resolution output
+        gen_hr = keras.layers.Conv2D(3, kernel_size=3, strides=1, padding='same', activation='tanh')(u2)
 
         return keras.models.Model(img_lr, gen_hr)
 
@@ -177,24 +221,21 @@ class FastSRGAN(object):
             if bn:
                 d = keras.layers.BatchNormalization(momentum=0.8)(d)
             d = keras.layers.LeakyReLU(alpha=0.2)(d)
-                
+
             return d
 
         # Input img
         d0 = keras.layers.Input(shape=self.hr_shape)
 
         d1 = d_block(d0, self.df, bn=False)
-        d2 = d_block(d1, self.df * 2, strides=2)
-        d3 = d_block(d2, self.df * 2)
-        d4 = d_block(d3, self.df * 4, strides=2)
-        d5 = d_block(d4, self.df * 4)
-        d6 = d_block(d5, self.df * 8, strides=2)
-        d7 = d_block(d6, self.df * 8)
+        d2 = d_block(d1, self.df, strides=2)
+        d3 = d_block(d2, self.df)
+        d4 = d_block(d3, self.df, strides=2)
+        d5 = d_block(d4, self.df * 2)
+        d6 = d_block(d5, self.df * 2, strides=2)
+        d7 = d_block(d6, self.df * 2)
+        d8 = d_block(d7, self.df * 2, strides=2)
 
-        # Flatten
-        d8 = keras.layers.Flatten()(d7)
-        d9 = keras.layers.Dense(1024)(d8)
-        d10 = keras.layers.LeakyReLU(alpha=0.2)(d9)
-        validity = keras.layers.Dense(1)(d10)
+        validity = keras.layers.Conv2D(1, kernel_size=1, strides=1, activation='sigmoid', padding='same')(d8)
 
         return keras.models.Model(d0, validity)
