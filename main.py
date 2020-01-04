33b2488df1a58e9c06ceab8e314028a193a955d9
Alexander <hasnain3257@gmail.com>
2020-01-04 14:19:19 -0800

    model

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from argparse import ArgumentParser
 from dataloader import DataLoader
 from model import FastSRGAN
 import tensorflow as tf
-import os
 
 parser = ArgumentParser()
 parser.add_argument('--image_dir', type=str, help='Path to high resolution image directory.')
 parser.add_argument('--batch_size', default=8, type=int, help='Batch size for training.')
-parser.add_argument('--epochs', default=1, type=int, help='Number of epochs for training')
+parser.add_argument('--epochs', default=100000, type=int, help='Number of epochs for training')
 parser.add_argument('--hr_size', default=384, type=int, help='Low resolution input size.')
 parser.add_argument('--lr', default=1e-4, type=float, help='Learning rate for optimizers.')
 parser.add_argument('--save_iter', default=200, type=int,
--- model.py
+++ model.py
@@ -173,8 +173,7 @@ class FastSRGAN(object):
             Returns:
                 u: Upsampled input by a factor of 2.
             """
-            u = keras.layers.Conv2D(filters, kernel_size=3, strides=1, padding='same')(layer_input)
-            u = tf.nn.depth_to_space(u, 2)
+            u = keras.layers.Conv2DTranspose(filters, kernel_size=3, strides=2, padding='same')(layer_input)
             u = keras.layers.PReLU(shared_axes=[1, 2])(u)
             return u
 
